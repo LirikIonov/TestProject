393db9d133e8185129e18793f3c23f9749a76a08
Kirill <48033224+LirikIonov@users.noreply.github.com>
2024-02-12 18:08:28 +0300

    Initial commit
    refactor

---
+++ src/ru/sgu/Main.java
@@ -0,0 +1,11 @@
+package ru.sgu;
+
+public class Main {
+    void run() {
+        System.out.println("Hellooooooo!!!");
+    }
+
+    public static void main(String[] args) {
+        new Main().run();
+    }
+}

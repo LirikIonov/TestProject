79b18fb446494b40a7b76bebf79e9921b4e5515f
LirikIonov <ghose100@yandex.ru>
2024-02-12 19:00:17 +0300

    feat: add long multiplication

--- src/ru/sgu/Main.java
+++ src/ru/sgu/Main.java
@@ -2,7 +2,10 @@ package ru.sgu;
 
 public class Main {
     void run() {
-        System.out.println("Hellooooooo!!!");
+        long x = 1000000;
+        long y = 21348;
+
+        System.out.println(x * y);
     }
 
     public static void main(String[] args) {
